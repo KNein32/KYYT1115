6cdd068561115926e3afa0e1972b9eb2c52398cc
KNein32 <KNein256@gmail.com>
2015-11-23 00:09:36 -0500

    Renamed the Alexa command to AlexaImport. Better understand what the file does.

--- app/Console/Commands/AlexaImport.php
+++ app/Console/Commands/AlexaImport.php
@@ -4,7 +4,7 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 
-class AlexaCommand extends Command
+class AlexaImport extends Command
 {
     /**
      * The name and signature of the console command.
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('AlexaCommand')
+        $schedule->command('AlexaImport')
                  ->daily();
     }
 }
