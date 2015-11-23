872221c22c169c2fc212c9496ae75d120ff54e18
KNein32 <KNein256@gmail.com>
2015-11-22 20:29:28 -0500

    Created Seeder for database.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,10 +12,32 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        Model::unguard();
-
-        // $this->call(UserTableSeeder::class);
-
-        Model::reguard();
+    	$id = DB::table('groups')->insertGetId([
+        'name' => 'admin',
+        'created_at' => date("Y-m-d H:i:s"),
+				'updated_at' => date("Y-m-d H:i:s"),
+      ]);
+			
+			DB::table('users')->insert([
+        'name' => 'admin',
+        'password' => '1234',
+        'groupId' => $id,
+        'created_at' => date("Y-m-d H:i:s"),
+        'updated_at' => date("Y-m-d H:i:s"),
+      ]);
+			
+			$id = DB::table('groups')->insertGetId([
+        'name' => 'user',
+        'created_at' => date("Y-m-d H:i:s"),
+        'updated_at' => date("Y-m-d H:i:s"),
+      ]);
+			
+			DB::table('users')->insert([
+        'name' => 'user',
+        'password' => 'asdf',
+        'groupId' => $id,
+        'created_at' => date("Y-m-d H:i:s"),
+        'updated_at' => date("Y-m-d H:i:s"),
+      ]);
     }
 }
